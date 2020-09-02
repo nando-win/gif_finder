bafc6e3cadb21411e19f96bcc7a05e044370847c
Fernando Bezerra <fernando.bezerra@atlantatecnologia.com.br>
2020-09-02 17:06:06 -0300

    Make function to get gifs

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -11,14 +13,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _search;
+  int _offSet = 0;
 
   String trendingsUrl =
       'https://api.giphy.com/v1/gifs/trending?api_key=WTiUHKO5XWmka034QGQslXW4cDPEuGJz&limit=25&rating=g';
 
-  _getGifs() async {
+  String searchUrl =
+      'https://api.giphy.com/v1/gifs/search?api_key=WTiUHKO5XWmka034QGQslXW4cDPEuGJz&q=golden&limit=25&offset=0&rating=g&lang=en';
+
+  Future<Map> _getGifs() async {
     http.Response response;
 
-    if (_search == null) response = await http.get(trendingsUrl);
+    if (_search == null)
+      response = await http.get(trendingsUrl);
+    else
+      response = await http.get(
+          'https://api.giphy.com/v1/gifs/search?api_key=WTiUHKO5XWmka034QGQslXW4cDPEuGJz&q=$_search&limit=25&offset=$_offSet&rating=g&lang=en');
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) => print(map));
   }
 
   @override
